e343e5ffdf6975a50d7bb5daac86a8887b9d87ba
stvnmoon <steven@imoon.tech>
2017-04-12 15:06:01 -0700

    can't get npm loaded module to work - trying different name

--- dist/index.js
+++ dist/index.js
@@ -560,4 +560,4 @@ module.exports = ReconnectingWebsocket;
 
 /***/ })
 /******/ ]);
-//# sourceMappingURL=vue-django-channels.js.map
+//# sourceMappingURL=index.js.map
--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: 'vue-django-channels.js'
+    filename: 'index.js'
   },
   module: {
     rules: [
