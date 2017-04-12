be095c5ec50a96a90b444810a6ae25e4a71248ac
stvnmoon <steven@imoon.tech>
2017-04-12 16:36:48 -0700

    removed .min from output file name

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -60,7 +60,6 @@ module.exports = {
 if (process.env.NODE_ENV === 'production') {
   module.exports.devtool = '#source-map'
   // http://vue-loader.vuejs.org/en/workflow/production.html
-  module.exports.output.filename = '[name].min.js',
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
